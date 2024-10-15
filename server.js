d6000a0e676e4aa57130c6131cc1402cb99937d3
claiv <claivent@gmail.com>
2024-10-15 12:01:14 +0200

    production

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the Angular app's dist directory
-app.use(express.static(__dirname + '/dist/<your-app-name>'));
+app.use(express.static(__dirname + '/dist/client'));
 
 // Redirect all other requests to the Angular app's index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/<your-app-name>/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/client/index.html'));
 });
 
 // Start the app on the default Heroku port
